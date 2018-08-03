67cb941da997e0b4a6315040d8e49279f953ce5d
tayyab80 <tayyab80@Outlook.com>
2018-08-03 16:54:16 +0500

    Reactive Forms Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { MatButtonModule } from '@angular/material/button';
 
 import 'hammerjs';
 
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 
 
@@ -36,6 +38,7 @@ import { LoginComponent } from './login/login.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -62,12 +65,15 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     MatCardModule,
     MatDialogModule,
     MatButtonModule,
+    MatSelectModule,
+    MatSlideToggleModule,
     FlexLayoutModule,
     MatFormFieldModule, 
     MatInputModule,
     MatCheckboxModule,
     MatListModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   entryComponents:[LoginComponent],
   providers: [DishService,PromotionService],
