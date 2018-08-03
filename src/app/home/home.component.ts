9e4f9b206a9353cf85b796bdbd9e5f6c4ca5f276
tayyab80 <tayyab80@Outlook.com>
2018-08-03 20:33:56 +0500

    Promise Part 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,8 +19,8 @@ export class HomeComponent implements OnInit {
     private promotionservice: PromotionService) { }
 
   ngOnInit() {
-    this.dish = this.dishservice.getFeaturedDish();
-    this.promotion = this.promotionservice.getFeaturedPromotion();
+    this.dishservice.getFeaturedDish().then(dish=>this.dish=dish);
+    this.promotionservice.getFeaturedPromotion().then(promotion=>this.promotion=promotion);
   }
 
 }
